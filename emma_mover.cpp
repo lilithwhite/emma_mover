0f187d105cfb714aebaffd620312f5727044f51e
lilithwhite <39236392+lilithwhite@users.noreply.github.com>
2022-03-28 20:19:16 +0300

    Update emma_mover.cpp

--- emma_mover.cpp
+++ emma_mover.cpp
@@ -12,12 +12,10 @@ int main (int argc, char *argv[]){
         cout << "aborted" << '\n';
         exit(0);
     }
-    string files [1000];
     string target_file = argv[1];
     string output_file = argv[2];
     string target = argv[3];
     string are_you_sure;
-    int file_number = 0;
     cout << "are you sure Y/n" << '\n';
     cin >> are_you_sure;
     if (are_you_sure != "Y") {
@@ -27,20 +25,12 @@ int main (int argc, char *argv[]){
     for (auto const& dir_entry : experimental::filesystem::recursive_directory_iterator(target_file)){
         string dir = experimental::filesystem::path(dir_entry);    
             if (dir.find(target) != -1) {
-                files [file_number] = dir;
-                file_number = file_number + 1;
+                std::size_t found = dir.find_last_of("/\\");
+                string test = output_file + dir.substr(found+1);
+                if(test != dir){
+                    experimental::filesystem::copy (dir, output_file, experimental::filesystem::copy_options::overwrite_existing |  experimental::filesystem::copy_options::recursive);
+                }
+
             }
     }
-    file_number = 0;
-    do
-    {
-        std::size_t found = files [file_number].find_last_of("/\\");
-        string test = output_file + files [file_number].substr(found+1);
-        if (test == files [file_number]) {
-            file_number = file_number + 1;
-        }else{
-            experimental::filesystem::copy (files [file_number], output_file, experimental::filesystem::copy_options::overwrite_existing | experimental::filesystem::copy_options::recursive);
-            file_number = file_number + 1;
-        }
-    }while (files [file_number].empty() == 0);
 }
